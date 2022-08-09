92a86c20c3a50064893fe4d5e5d79f2d170bdeb2
Mir Imad Ahmed <mirimadahmed@outlook.com>
2022-08-09 20:02:30 +0500

    ha

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,14 +5,14 @@ module.exports = {
       env: {
         NODE_ENV: "development",
         BINDIP: "0.0.0.0",
-        PORT: 443,
+        PORT: 1234,
         MONITORING: true
 
       },
       env_production: {
         NODE_ENV: "production",
         BINDIP: "0.0.0.0",
-        PORT: 443,
+        PORT: 1234,
         MONITORING: true
       }
     }]
