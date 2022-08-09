0f8d79b81340036d82899b3ef94234813bfe938c
Mir Imad Ahmed <mirimadahmed@outlook.com>
2022-08-09 19:58:31 +0500

    f

--- app/config/config.js
+++ app/config/config.js
@@ -4,9 +4,9 @@ module.exports = {
 
   /** DATABASE */
   db: {
-    DB_HOST: 'nftprdd01aw.ctymd45abzdi.ap-east-1.rds.amazonaws.com',
-    DB_USER: 'tgsysadm',
-    DB_PASS: 'tMt34aAbDgEPwGXR',
+    DB_HOST: 'database-1.ca0bdz22c1lh.eu-central-1.rds.amazonaws.com',
+    DB_USER: 'admin',
+    DB_PASS: 'admin-123',
     DB_NAME: 'towngas',
     dialect: 'mysql',
   },
